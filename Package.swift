57c052b37700492e82fd4beb1a95b264d8320ef1
Helge Heß <devteam@zeezide.de>
2022-08-10 17:27:54 +0200

    Use Lighter 1.0.0
    The release.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ var package = Package(
   
   dependencies: [
     .package(url: "git@github.com:55DB091A-8471-447B-8F50-5DFF4C1B14AC/Lighter.git",
-             branch: "develop"),
+             from: "1.0.0"),
     .package(url: "https://github.com/stephencelis/SQLite.swift", 
              from: "0.9.2"),
     .package(url: "https://github.com/groue/GRDB.swift", from: "5.26.0")
